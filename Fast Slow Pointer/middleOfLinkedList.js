60fde744a6970c887c21a5f137f11a1842668d7e
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-06-28 17:59:28 +0530

    Added - Another solution for finding middle element of LinkedList

--- Fast Slow Pointer/middleOfLinkedList.js
+++ Fast Slow Pointer/middleOfLinkedList.js
@@ -12,7 +12,7 @@ class Node{
     }
 }
 
-
+// first solution 
 function findMiddleOfLinkedlist(head){
     let current = head
     let linkedListLength = 0
@@ -32,12 +32,25 @@ function findMiddleOfLinkedlist(head){
 
 }
 
+// Second Solution
+
+function middleOfLinkedList(head){
+    let slow = head 
+    let fast = head 
+
+    while(fast !== null && fast.next !== null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow
+}
+
 
 const head = new Node (1)
 head.next = new Node(2)
 head.next.next = new Node(3)
 head.next.next.next = new Node(4)
 head.next.next.next.next = new Node(5)
-// head.next.next.next.next.next = new Node(6)
+head.next.next.next.next.next = new Node(6)
 
-console.log(`the middle of Linkedlist ===> ${findMiddleOfLinkedlist(head)}`)
+console.log(`the middle of Linkedlist ===> ${middleOfLinkedList(head).value}`)
