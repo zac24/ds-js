f66d2228c7ac4a96d42615b6cb0708eefee0c0d2
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-07-06 15:11:33 +0530

    added NoRepeat Substr

---
+++ sliding window/noRepeatSubstr.js
@@ -0,0 +1,40 @@
+/* 
+
+// Given a string, find the length of the longest substring which has no repeating characters.
+
+Example 1:
+Input: String="aabccbb"
+Output: 3
+Explanation: The longest substring without any repeating characters is "abc".
+
+Example 2:
+Input: String="abbbb"
+Output: 2
+Explanation: The longest substring without any repeating characters is "ab".
+
+Example 3:
+Input: String="abccde"
+Output: 3
+Explanation: Longest substrings without any repeating characters are "abc" & "cde".
+
+*/
+
+function noRepeatSubstr(str){
+    let start = 0
+    let charMap = {}
+    let maxLength = 0
+    const inputArr = str.split('')
+    for(end = 0; end < inputArr.length; end++){
+        const rightChar = inputArr[end]
+        if(rightChar in charMap){
+            start = Math.max(start, charMap[rightChar] + 1)
+        }
+        charMap[rightChar] = end
+        maxLength = Math.max(maxLength, end - start + 1)
+    }
+    return maxLength 
+}
+
+console.log(`Length of longest Substring: ${noRepeatSubstr('aabccbb')}`)
+console.log(`Length of longest Substring: ${noRepeatSubstr('abbbb')}`)
+console.log(`Length of longest Substring: ${noRepeatSubstr('abccde')}`)
