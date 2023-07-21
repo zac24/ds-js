621b7e56319d4eb5861a96aa5b2c07e32b5453bf
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-07-21 18:24:50 +0530

    Added: Palindrome using stack

--- LinkedList/Palindrome.js
+++ LinkedList/Palindrome.js
@@ -11,21 +11,46 @@ class Node{
     }
 }
 // Extreme easy solution
-function isPallindromeList(head){
-    let string1 = string2 = ''
-    let current = head
-    while(current !== null){
-        string1 = `${string1}${current.value}`
-        string2 = `${current.value}${string2}`
+// function isPallindromeList(head){
+//     let string1 = string2 = ''
+//     let current = head
+//     while(current !== null){
+//         string1 = `${string1}${current.value}`
+//         string2 = `${current.value}${string2}`
 
+//         current = current.next
+//     }
+//     return string1 === string2
+// }
+
+
+
+// ========== Using Stack ==========
+
+function isPalindromeusingStack(head){
+    let stack = []
+    let current = head
+
+    while(current != null){
+        stack.push(current.value)
         current = current.next
     }
-    return string1 === string2
+
+    while(head !== null){
+        let element = stack.pop()
+        if(element !== head.value){
+            return false
+        } else{
+            head = head.next
+        }
+    }
+    return true
 }
 
-const head = new Node (1)
-head.next = new Node(2)
-head.next.next = new Node(2)
-head.next.next.next = new Node(1)
+const head = new Node('1')
+head.next = new Node('2')
+head.next.next = new Node('3')
+head.next.next.next = new Node('2')
+head.next.next.next.next = new Node('1')
 
-console.log(isPallindromeList(head))
+console.log(isPalindromeusingStack(head))
