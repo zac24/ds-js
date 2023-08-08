9b29600d957c7f3efbf88438d9b14d08aa263b32
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-08-08 16:24:26 +0530

    Added: remove elements matches with given value

--- LinkedList/questions.js
+++ LinkedList/questions.js
@@ -9,10 +9,10 @@
  * How do you find the middle element of a singly linked list in one pass? (solution) - Done
  * How do you check if a given linked list contains a cycle? How do you find the starting node of the cycle? (solution) - Done
  * How do you find the third node from the end in a singly linked list? (solution) -- Done
- * How do you find the sum of two linked lists using Stack? (solution) - Partially Done
+ * How do you find the sum of two linked lists using Stack? (solution) - Done
  * How do you reverse a linked list in place? (solution) - Done
  * How to add an element in the middle of the linked list? (solution) - Done
- * How do you sort a linked list in Java? (solution) 
+ * How do you sort a linked list in Java? (solution) - Done
  * What is the difference between the array and linked list? (answer)
  * How to remove Nth Node from the end of a linked list? (solution) - Done
  * How to merge two sorted linked lists? (solution) - Done
@@ -20,7 +20,7 @@
  * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x. (solution) - Done
  * How to remove duplicates from a sorted linked list? (solution) - Done
  * How to find the node at which the intersection of two singly linked lists begins. (solution) - Done
- * How to check if a given linked list is a palindrome? (solution)
+ * How to check if a given linked list is a palindrome? (solution) - Done
  * How to remove all elements from a linked list of integers which match with given value? (solution)
  * How do you reverse a linked list? (solution)
  * How do you reverse a singly linked list without recursion? (solution)
---
+++ LinkedList/removeElementFromList.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * How to remove all elements from a linked list of integers which match with given value?
+ * 
+ */
+
+
+function removeElementsMatchesWithGivenInput(head, element){
+    let current = head
+
+    while(current != null){
+        
+    }
+}
